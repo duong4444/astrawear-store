43670025571ca7b87ca350fd46a8392af179907d
Duong <duongkc4444@gmail.com>
2025-09-02 21:58:04 +0700

    related product

--- app/(root)/products/[productId]/page.tsx
+++ app/(root)/products/[productId]/page.tsx
@@ -1,11 +1,11 @@
 import Gallery from "@/components/Gallery"
-// import ProductCard from "@/components/ProductCard"
+import ProductCard from "@/components/ProductCard"
 import ProductInfo from "@/components/ProductInfo"
 import { getProductDetails, getRelatedProducts } from "@/lib/actions/actions"
 
 const ProductDetails = async ({ params }: { params: { productId: string }}) => {
   const productDetails = await getProductDetails(params.productId)
-  // const relatedProducts = await getRelatedProducts(params.productId)
+  const relatedProducts = await getRelatedProducts(params.productId)
 
   return (
     <>
@@ -14,14 +14,14 @@ const ProductDetails = async ({ params }: { params: { productId: string }}) => {
       <ProductInfo productInfo={productDetails} />
     </div>
 
-    {/* <div className="flex flex-col items-center px-10 py-5 max-md:px-3">
+    <div className="flex flex-col items-center px-10 py-5 max-md:px-3">
       <p className="text-heading3-bold">Related Products</p>
       <div className="flex flex-wrap gap-16 mx-auto mt-8">
         {relatedProducts?.map((product: ProductType) => (
           <ProductCard key={product._id} product={product} />
         ))}
       </div>
-    </div> */}
+    </div>
     </>
   )
 }
